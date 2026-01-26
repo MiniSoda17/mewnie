f076120d02e3bfe8b385222e453696836682038c
MiniSoda17 <isaac.arli@gmail.com>
2026-01-26 15:17:37 +1000

    Something

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -41,7 +41,6 @@ export default function Home() {
       <main className="pt-20">
         {/* Hero Section */}
         <section className="w-full relative">
-          <FadeIn delay={0.1} className="w-full">
             <div className="relative w-full">
               <Image
                 src="/frontpage.png"
@@ -54,12 +53,10 @@ export default function Home() {
                 unoptimized
               />
             </div>
-          </FadeIn>
         </section>
 
         {/* River Background Section */}
         <section className="w-full relative">
-          <FadeIn delay={0} className="w-full">
             <div className="relative w-full">
               <Image
                 src="/secondpagefinal2.0.png"
@@ -81,7 +78,6 @@ export default function Home() {
                 />
               </div>
             </div>
-          </FadeIn>
         </section>
 
         {/* How It Works Section */}
@@ -144,7 +140,6 @@ export default function Home() {
 
         {/* CTA */}
         <section className="py-20 px-6">
-          <FadeIn>
             <div className="w-full max-w-5xl mx-auto relative rounded-3xl md:rounded-[3rem] overflow isolate flex flex-col justify-center items-center text-center py-17 px-4 md:py-32 transform-gpu [backface-visibility:hidden] [will-change:transform]">
               <Image
                 src="/sleepyforest.png"
@@ -154,18 +149,21 @@ export default function Home() {
                 unoptimized
                 priority
               />
-              <h2 className="text-2xl md:text-5xl font-bold text-white mb-4 md:mb-6 drop-shadow-lg">
-                Join the evolution
-              </h2>
+              <FadeIn>
+                <h2 className="text-2xl md:text-5xl font-bold text-white mb-4 md:mb-6 drop-shadow-lg">
+                  Join the evolution
+                </h2>
+              </FadeIn>
               
-              <Link
-                href="/signup"
-                className="px-8 py-4 mb-30 md:px-10 md:py-5 rounded-full bg-white text-black font-bold text-lg md:text-xl hover:bg-gray-100 transition-transform hover:scale-105 inline-block shadow-lg"
-              >
-                Join Beta Waitlist
-              </Link>
+              <FadeIn delay={0.1}>
+                <Link
+                  href="/signup"
+                  className="px-8 py-4 mb-30 md:px-10 md:py-5 rounded-full bg-white text-black font-bold text-lg md:text-xl hover:bg-gray-100 transition-transform hover:scale-105 inline-block shadow-lg"
+                >
+                  Join Beta Waitlist
+                </Link>
+              </FadeIn>
             </div>
-          </FadeIn>
         </section>
       </main>
 
